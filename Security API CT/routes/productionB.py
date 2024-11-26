37cb198e0f44a9f9e34d0e496721a71540c8ba56
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-25 18:57:05 -0600

    Update productionB.py

--- Security API CT/routes/productionB.py
+++ Security API CT/routes/productionB.py
@@ -3,5 +3,6 @@ from controllers.productionController import save, find_all
 
 production_blueprint = Blueprint("production_bp", __name__)
 
+
 production_blueprint.route('/', methods=['POST'])(save)
 production_blueprint.route('/', methods=['GET'])(find_all)
